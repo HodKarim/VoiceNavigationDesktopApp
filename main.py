f5c3c2233947257d1dbdd7424e7ddc70a9151847
Hodo <hodkarim64@gmail.com>
2025-07-26 11:47:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,10 @@
-#iterable - an object/collection that can b iterated thru a loop
+
 import time
 from vosk import Model, KaldiRecognizer
 import pyaudio 
 #lists [], tuples (), sets {} are all iterable
 #strings are also iterable
 #dictionaries are iteranbe
-
-#membership operators (in or not in) (string, list, tuple, set, dict)
-
-#list comprehension in python = concise way to create lists
-#for every value in an interable check if works
-
-#formula = expression for value in iterable if condition <- optiona;
-
-#matchcase statements or switch: execute code if value == case
-#match name: case 1: .... case 2: .... case _: ...
-
-#modules: a file containing code u want in ur program
-#eg import time, math, etc
-
-#variable scope: LEGB (local, enclosed, global, built in)
-
-#if name==main:..if we dont run this program directly, dont do it
-
-#object = a bundle of related attributes (variables) and 
-#  methods (functions)
-# object =  cup is object (liquid inside, temperature, attributes)
-#           methods for cup is fill, drink and empty.
-#           class is a blueprint used to design the structure and layout of object
-
 
 model = Model(r"C:\Users\ayaan\OneDrive\Desktop\Assistive Project\vosk-model-small-en-us-0.15\vosk-model-small-en-us-0.15")
 recognizer = KaldiRecognizer(model, 16000)
